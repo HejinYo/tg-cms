da550d88694440b414373bab7c4ef4f2b99c374c
Administrator <Administrator@SD-201607171157>
2016-08-18 13:49:11 +0800

    常规维护 不要更新

--- tg-cms/src/main/java/com/turingoal/cms/core/commons/TgSecurityAuthenticationEntryPoint.java
+++ tg-cms/src/main/java/com/turingoal/cms/core/commons/TgSecurityAuthenticationEntryPoint.java
@@ -1,6 +1,8 @@
 package com.turingoal.cms.core.commons;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,42 +11,33 @@ import org.springframework.security.web.authentication.LoginUrlAuthenticationEnt
 import com.turingoal.common.bean.JsonResultBean;
 import com.turingoal.common.util.net.RequestUtil;
 import com.turingoal.common.util.net.WebUtils;
+import com.turingoal.common.util.spring.SpringSecurityDirectUrlResolver;
 
 /**
- * 登录入口。 默认的不支持java，需要自定义
+ * 登录入口。 默认的不支持javascript，需要自定义
  */
 public class TgSecurityAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
+    private String defaultLoginPageUrl = "/loginPage.htm"; // 默认登录页面
+    private List<SpringSecurityDirectUrlResolver> directUrlResolvers = new ArrayList<SpringSecurityDirectUrlResolver>(); // 多登陆页面
 
     public TgSecurityAuthenticationEntryPoint(final String loginFormUrl) {
         super(loginFormUrl);
-    }
-
-    private String foreLoginPageUrl = "/loginPage.htm"; // 前台登录页面
-    private String adminLoginPageUrl = "/admin/loginPage.gsp"; // 后台登录页面
-
-    public void setForeLoginPageUrl(final String foreLoginPageUrlParm) {
-        this.foreLoginPageUrl = foreLoginPageUrlParm;
-    }
-
-    public void setAdminLoginPageUrl(final String adminLoginPageUrlParm) {
-        this.adminLoginPageUrl = adminLoginPageUrlParm;
+        defaultLoginPageUrl = loginFormUrl; // 默认登录页面赋值
     }
 
     @Override
     public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException) throws IOException, ServletException {
-        boolean isAjax = RequestUtil.isAjax(request);
+        boolean isAjax = RequestUtil.isAjax(request); // 是否Ajax请求
         boolean hasSession = SystemHelper.isAuthenticated();
         if (isAjax && !hasSession) {
             this.transformAjaxRequest(request, response);
         } else {
-            String targetUrl = null;
-            String url = request.getRequestURI();
-            if (url.indexOf("admin") != -1) {
-                // 未登录而访问后台受控资源时，跳转到后台登录页面
-                targetUrl = adminLoginPageUrl;
-            } else {
-                // 未登录而访问前台受控资源时，跳转到前台登录页面
-                targetUrl = foreLoginPageUrl;
+            String targetUrl = defaultLoginPageUrl; // 每次先恢复为默认的
+            // 多登录页面处理
+            for (SpringSecurityDirectUrlResolver directUrlResolver : directUrlResolvers) {
+                if (directUrlResolver.support(request)) {
+                    targetUrl = directUrlResolver.directUrl();
+                }
             }
             targetUrl = request.getContextPath() + targetUrl;
             response.sendRedirect(targetUrl);
@@ -57,5 +50,9 @@ public class TgSecurityAuthenticationEntryPoint extends LoginUrlAuthenticationEn
      */
     private void transformAjaxRequest(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
         WebUtils.writeToBrowser(new JsonResultBean("Session超时，请重新登录").toString(), response);
+    }
+
+    public void setDirectUrlResolvers(final List<SpringSecurityDirectUrlResolver> directUrlResolversParm) {
+        this.directUrlResolvers = directUrlResolversParm;
     }
 }
--- tg-cms/src/main/java/com/turingoal/cms/core/filter/SystemInitListener.java
+++ tg-cms/src/main/java/com/turingoal/cms/core/filter/SystemInitListener.java
@@ -13,32 +13,32 @@ import com.turingoal.common.license.TgLicenseChcker;
  * 系统初始化监听器
  */
 public class SystemInitListener implements ApplicationListener<ContextRefreshedEvent>, ServletContextAware {
-	private final Logger log = LoggerFactory.getLogger(SystemInitListener.class);
-	private ServletContext application;
-	@Value("#{applicationProperties['security.openValidateCaptchaCode']}")
-	private Boolean openValidateCaptchaCode; // 是否开启验证码
-	@Value("#{applicationProperties['security.openValidateCaptchaCode4admin']}")
-	private Boolean openValidateCaptchaCode4admin; // 后台是否开启验证码
+    private final Logger log = LoggerFactory.getLogger(SystemInitListener.class);
+    private ServletContext application;
+    @Value("#{applicationProperties['security.openValidateCaptchaCode']}")
+    private Boolean openValidateCaptchaCode; // 是否开启验证码
+    @Value("#{applicationProperties['security.openValidateCaptchaCode4admin']}")
+    private Boolean openValidateCaptchaCode4admin; // 后台是否开启验证码
 
-	/**
-	 * 在Spring容器所有的Bean都初始化完成之后执行
-	 */
-	@Override
-	public void onApplicationEvent(final ContextRefreshedEvent event) {
-		// 在web 项目中（spring mvc），系统会存在两个容器，一个是root application context ,另一个就是我们自己的 context（作为root application context的子容器）。 这种情况下，就会造成onApplicationEvent方法被执行两次。为了避免上面提到的问题，我们可以只在root application context初始化完成后调用逻辑代码，其他的容器的初始化完成，则不做任何处理
-		if (event.getApplicationContext().getParent() == null) {
-			log.info("系统启动");
-			// 校验license
-			TgLicenseChcker.checkInit(application, SystemInitListener.class);
-			// 是否开启验证码
-			application.setAttribute("openValidateCaptchaCode", openValidateCaptchaCode);
-			// 后台是否开启验证码
-			application.setAttribute("openValidateCaptchaCode4admin", openValidateCaptchaCode4admin);
-		}
-	}
+    /**
+     * 在Spring容器所有的Bean都初始化完成之后执行
+     */
+    @Override
+    public void onApplicationEvent(final ContextRefreshedEvent event) {
+        // 在web 项目中（spring mvc），系统会存在两个容器，一个是root application context ,另一个就是我们自己的 context（作为root application context的子容器）。 这种情况下，就会造成onApplicationEvent方法被执行两次。为了避免上面提到的问题，我们可以只在root application context初始化完成后调用逻辑代码，其他的容器的初始化完成，则不做任何处理
+        if (event.getApplicationContext().getParent() == null) {
+            log.info("系统启动");
+            // 校验license
+            TgLicenseChcker.checkInit(application, SystemInitListener.class);
+            // 是否开启验证码
+            application.setAttribute("openValidateCaptchaCode", openValidateCaptchaCode);
+            // 后台是否开启验证码
+            application.setAttribute("openValidateCaptchaCode4admin", openValidateCaptchaCode4admin);
+        }
+    }
 
-	@Override
-	public void setServletContext(final ServletContext servletContext) {
-		this.application = servletContext;
-	}
+    @Override
+    public void setServletContext(final ServletContext servletContext) {
+        this.application = servletContext;
+    }
 }
