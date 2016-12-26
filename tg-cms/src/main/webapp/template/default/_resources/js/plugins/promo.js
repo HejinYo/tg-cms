cc4c44015dda67028efae32a5c47c1e071e6bbbd
Administrator <Administrator@SD-201607171157>
2016-12-26 17:35:40 +0800

    常规维护 不要更新

--- tg-cms/src/main/webapp/template/default/_resources/js/plugins/promo.js
+++ tg-cms/src/main/webapp/template/default/_resources/js/plugins/promo.js
@@ -1,4 +1,4 @@
 $(function() {
-  $(".fullscreen-static-image").backstretch(["assets/img-temp/promo/img1.jpg", "assets/img-temp/promo/img2.jpg"],
+  $(".fullscreen-static-image").backstretch(["template/default/_resources/imgs/temp/img1.jpg", "template/default/_resources/imgs/temp/img2.jpg"],
   {duration: 5000, fade:1500});
 });
