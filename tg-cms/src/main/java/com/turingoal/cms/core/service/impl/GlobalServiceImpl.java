c856e7d97a705b09f952c986dab693a94d97884e
Administrator <Administrator@SD-201607171157>
2016-12-28 18:15:24 +0800

    常规维护 不要更新

--- tg-cms/src/main/java/com/turingoal/cms/core/service/impl/GlobalServiceImpl.java
+++ tg-cms/src/main/java/com/turingoal/cms/core/service/impl/GlobalServiceImpl.java
@@ -1,6 +1,8 @@
 package com.turingoal.cms.core.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import com.turingoal.cms.core.commons.SystemHelper;
 import com.turingoal.cms.core.domain.Global;
@@ -20,6 +22,7 @@ public class GlobalServiceImpl implements GlobalService {
     /**
      * 获取全局配置
      */
+    @Cacheable(value = "sys-global")
     @MethodLog(name = "获取全局配置", description = "获取全局配置")
     public Global get() {
         return globalDao.get();
@@ -28,6 +31,7 @@ public class GlobalServiceImpl implements GlobalService {
     /**
      * 修改 全局参数
      */
+    @CacheEvict(value = "sys-global")
     @MethodLog(name = "修改全局配置", description = "修改全局配置")
     public int update(final GlobalForm form) {
         form.setUpdateDataUsername(SystemHelper.getCurrentUsername());
